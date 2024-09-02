1127ad912f527e85fcf88edd2d953479f709cd68
Jérome Eertmans <jeertmans@icloud.com>
2024-09-02 15:12:59 +0200

    chore(lib): concrete class, smarter type hints and patch options

--- src/pytest_missing_modules/plugin.py
+++ src/pytest_missing_modules/plugin.py
@@ -4,11 +4,12 @@ from __future__ import annotations
 
 import builtins
 import importlib
+import importlib.util
 import sys
-from contextlib import AbstractContextManager, contextmanager
+from contextlib import contextmanager
 from functools import wraps
 from threading import Lock
-from typing import TYPE_CHECKING, Protocol
+from typing import TYPE_CHECKING
 
 import pytest
 
@@ -16,41 +17,113 @@ if TYPE_CHECKING:
     from collections.abc import Iterator
     from typing import (
         Callable,
-        Concatenate,
-        ParamSpec,
-        TypeVar,
     )
+
+    if sys.version_info >= (3, 10):
+        from typing import Concatenate, ParamSpec, TypeVar
+    else:
+        from typing_extension import Concatenate, ParamSpec, TypeVar
 
     P = ParamSpec("P")
     R = TypeVar("R")
 
 
-class MissingModulesContextGenerator(Protocol):
-    """Context manager that raises ImportError for a series of modules.
+_LOCK = Lock()
+"""Lock used to make sure that :func:`missing_modules` is compatible
+with :mod:`pytest-xdist`."""
+
+
+class MissingModulesContextGenerator:
+    """Context manager generator that raises ImportError for a series of modules.
 
     In the provided context, an import of any modules in that list
     will raise an :py:class:`ImportError`.
     """
 
-    def __call__(
-        self, *names: str, error_msg: str = "Mocked import error for '{name}'"
-    ) -> AbstractContextManager[pytest.MonkeyPatch]:
+    def __init__(self, monkeypatch: pytest.MonkeyPatch) -> None:
+        """Instantiate a context manager generator.
+
+        Args:
+            monkeypatch: The monkeypatch object used to perform
+                all patches.
+        """
+        self.monkeypatch = monkeypatch
+
+    @contextmanager
+    def __call__(  # noqa: C901
+        self,
+        *names: str,
+        error_msg: str = "Mocked import error for '{name}'",
+        patch_import: bool = True,
+        patch_find_spec: bool = True,
+    ) -> Iterator[pytest.MonkeyPatch]:
         """Enter the context manager.
 
         Args:
             names: A list of modules names.
             error_msg: A string template for import errors.
+            patch_import: Whether to patch :func:`import` and
+                :func:`importlib.utils.import_module`.
+            patch_find_spec: Whether to patch
+                :func:`importlib.util.find_spec`.
 
         Yields:
             A monkeypatch instance that mocks imports of the specified
             modules.
         """
-        ...
+        real_import = builtins.__import__
+        real_import_module = importlib.import_module
+        real_find_spec = importlib.util.find_spec
 
+        def should_mock(name: str) -> bool:
+            return name.partition(".")[0] in names
 
-_LOCK = Lock()
-"""Lock used to make sure that :func:`missing_modules` is compatible
-with :mod:`pytest-xdist`."""
+        def mock_import_func(
+            import_func: Callable[Concatenate[str, P], R],
+        ) -> Callable[Concatenate[str, P], R]:
+            @wraps(import_func)
+            def wrapper(name: str, *args: P.args, **kwargs: P.kwargs) -> R:
+                if should_mock(name):
+                    msg = error_msg.format(name=name)
+                    raise ImportError(msg)
+                return import_func(name, *args, **kwargs)
+
+            return wrapper
+
+        def mock_find_spec_func(
+            find_spec_func: Callable[Concatenate[str, P], R],
+        ) -> Callable[Concatenate[str, P], R | None]:
+            @wraps(find_spec_func)
+            def wrapper(name: str, *args: P.args, **kwargs: P.kwargs) -> R | None:
+                if should_mock(name):
+                    return None
+                return find_spec_func(name, *args, **kwargs)
+
+            return wrapper
+
+        with self.monkeypatch.context() as m, _LOCK:
+            module_names = tuple(sys.modules.keys())
+
+            for module_name in module_names:
+                if module_name.partition(".")[0] in names:
+                    m.delitem(sys.modules, module_name)
+
+            if patch_import:
+                m.setattr(builtins, "__import__", mock_import_func(real_import))
+                m.setattr(
+                    importlib,
+                    "import_module",
+                    mock_import_func(real_import_module),
+                )
+
+            if patch_find_spec:
+                m.setattr(
+                    importlib.util,
+                    "find_spec",
+                    mock_find_spec_func(real_find_spec),
+                )
+
+            yield m
 
 
 @pytest.fixture
@@ -61,39 +134,6 @@ def missing_modules(monkeypatch: pytest.MonkeyPatch) -> MissingModulesContextGen
         monkeypatch: A monkeypatch fixture, provided by: mod:`pytest`.
 
     Returns:
-        A context manager that can be used to create missing_modules contexts.
+        A context manager that can be used to create missing modules contexts.
     """
-
-    @contextmanager
-    def ctx(
-        *names: str,
-        error_msg: str = "Mocked import error for '{name}'",
-    ) -> Iterator[pytest.MonkeyPatch]:
-        real_import = builtins.__import__
-        real_import_module = importlib.import_module
-
-        def mock_import_func(
-            import_func: Callable[Concatenate[str, P], R],
-        ) -> Callable[Concatenate[str, P], R]:
-            @wraps(import_func)
-            def wrapper(name: str, *args: P.args, **kwargs: P.kwargs) -> R:
-                if name.partition(".")[0] in names:
-                    msg = error_msg.format(name=name)
-                    raise ImportError(msg)
-                return import_func(name, *args, **kwargs)
-
-            return wrapper
-
-        with monkeypatch.context() as m, _LOCK:
-            module_names = tuple(sys.modules.keys())
-
-            for module_name in module_names:
-                if module_name.partition(".")[0] in names:
-                    m.delitem(sys.modules, module_name)
-
-            m.setattr(builtins, "__import__", mock_import_func(real_import))
-            m.setattr(importlib, "import_module", mock_import_func(real_import_module))
-
-            yield m
-
-    return ctx
+    return MissingModulesContextGenerator(monkeypatch)
